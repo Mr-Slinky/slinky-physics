29703c4c8f83713fb0ff6a8888cd82a49f593ab1
Kheagen Haskins <slinky.haskins@gmail.com>
2024-09-24 14:00:11 +0200

    feat: Add SpringManager to Engine class

--- src/main/java/com/slinky/physics/base/Engine.java
+++ src/main/java/com/slinky/physics/base/Engine.java
@@ -6,6 +6,7 @@ import com.slinky.physics.components.ForceManager;
 import com.slinky.physics.components.MassManager;
 import com.slinky.physics.components.PositionManager;
 import com.slinky.physics.components.RestitutionManager;
+import com.slinky.physics.components.SpringManager;
 import com.slinky.physics.components.VelocityManager;
 
 import com.slinky.physics.entities.Archetype;
@@ -21,11 +22,11 @@ import java.util.Map;
  * A facade acting as the main controller of all physics systems.
  *
  * @version 0.1
- * @since  0.1.0
+ * @since   0.1.0
  * 
  * @author Kheagen Haskins
  */
-public class Engine {
+public final class Engine {
 
     // ============================== Static ================================ //
     public static final int VECTOR_MASK = Component.POSITION.bit()
@@ -39,38 +40,58 @@ public class Engine {
 
     // ============================== Fields ================================ //
     // Component Manager is a marker interface; it has no methods defined by that can change
-    private EntityManager entityManager;
-    private List<ISystem> systems                     = new ArrayList<>();
+    private EntityManager entMan;
+    private List<ISystem> systems = new ArrayList<>();
     private Map<Component, ComponentManager> managers = new HashMap<>();
 
     // =========================== Constructors ============================= //
     public Engine(EntityManager entityManager) {
-        this.entityManager = entityManager;
+        this.entMan = entityManager;
         
         // This is going to become incredibly bloated as more components are added... Hmmmm!
-        managers.put(Component.POSITION, new PositionManager   (entityManager, 16, entityManager.getEntityCapacity()));
-        managers.put(Component.VELOCITY, new VelocityManager   (entityManager, 16, entityManager.getEntityCapacity()));
-        managers.put(Component.FORCE,    new ForceManager      (entityManager, 16, entityManager.getEntityCapacity()));
-        managers.put(Component.MASS,     new MassManager       (entityManager, 16, entityManager.getEntityCapacity()));
-        managers.put(Component.MASS,     new RestitutionManager(entityManager, 16, entityManager.getEntityCapacity()));
+        managers.put(Component.POSITION,    new PositionManager   (entityManager, 16, entityManager.getEntityCapacity()));
+        managers.put(Component.VELOCITY,    new VelocityManager   (entityManager, 16, entityManager.getEntityCapacity()));
+        managers.put(Component.FORCE,       new ForceManager      (entityManager, 16, entityManager.getEntityCapacity()));
+        // scalar managers
+        managers.put(Component.MASS,        new MassManager       (entityManager, 16, entityManager.getEntityCapacity()));
+        managers.put(Component.RESTITUTION, new RestitutionManager(entityManager, 16, entityManager.getEntityCapacity()));
+        // special
+        managers.put(Component.SPRING,      new SpringManager(entityManager, 16, entityManager.getEntityCapacity()));
     }
     
     // ============================== Getters =============================== //
+    public int createPointMass() {
+        return createEntity(Archetype.POINT_MASS);
+    }
+    
+    public int createSpring(int pointMassId1, int pointMassId2, float restLength, float springConstant) {
+        boolean validEntity = isPointMass(pointMassId1) && isPointMass(pointMassId2);
+        if (!validEntity) {
+            throw new IllegalArgumentException(String.format("Entity %d and %d must both be Point Mass archetypes", pointMassId1, pointMassId2));
+        }
+        
+        int entId = entMan.createEntity();
+        SpringManager springMan = (SpringManager) managers.get(Component.SPRING);
+        springMan.add(entId, pointMassId1, pointMassId2, restLength, springConstant);
+        entMan.addComponentTo(entId, Component.SPRING);
+        return entId;
+    }
+    
     public int createEntity(Archetype archetype) {
-        int entityId = entityManager.createEntity(archetype);
+        int entId = entMan.createEntity(archetype);
         
         ComponentManager compMan;
         for (Component comp : archetype.components()) {
             compMan = managers.get(comp);
-            compMan.add(entityId);
+            compMan.add(entId);
         }
         
-        return entityId;
+        return entId;
     }
     
     public boolean destroyEntity(int entityId) {
-        int components       = entityManager.getComponentMask(entityId);
-        boolean wasDestroyed = entityManager.destroyEntity(entityId);
+        int components       = entMan.getComponentMask(entityId);
+        boolean wasDestroyed = entMan.destroyEntity(entityId);
         
         if (wasDestroyed) {
             for (ComponentManager compMan : managers.values()) {
@@ -90,9 +111,8 @@ public class Engine {
     }
     
     // ========================== Helper Methods ============================ //
-    @Override
-    public String toString() {
-        return super.toString();
+    private boolean isPointMass(int entityID) {
+        return Archetype.POINT_MASS.contains(entMan.getComponentMask(entityID));
     }
     
 }
